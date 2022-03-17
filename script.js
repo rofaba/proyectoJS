430acaa24d9cfaf55696799639a6e88970d9bb04
rofaba <rodrigofaure@gmail.com>
2022-03-17 17:26:30 -0300

    comentarios

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 //Simulador interactivo JS: Juego de "El Colgado".
-
 // DESAFIO COMPLEMENTARIO 2 INCLUYENDO OBJETOS FUNCIONES ARRAYS Y METODOS
 
 //creación de personajes .
-
 class Personaje {
     constructor(nombre, edad, ocupacion) {
         this.nombre = nombre;
         this.edad = edad;
-        this.ocupacion = ocupacion; 
+        this.ocupacion = ocupacion;
     }
 }
-const perso1 = new Personaje ('Carlos', ' 20 años ', ' Jugador Empedernido');
-const perso2 = new Personaje ('Roberto', ' 15 años ', ' Estudiante');
-const perso3 = new Personaje ('Claudia ', ' 29 años ', ' Profesora');
-
+const perso1 = new Personaje('Carlos', ' 20 años ', ' Jugador Empedernido');
+const perso2 = new Personaje('Roberto', ' 15 años ', ' Estudiante');
+const perso3 = new Personaje('Claudia ', ' 29 años ', ' Profesora');
 const poolPersonajes = [perso1, perso2, perso3];
 
 function saludaUsuario() {
-    alert(`Hola Pablo, hoy tengo algo nuevo que mostrarte`)
+    alert(`Hola Amigo, hoy tengo algo nuevo que mostrarte`)
 }
 function indexRandom(minimo, maximo) {   //los argumentos se establerán entre 0 y el número total de palabras 
-    var numerosPosibles = maximo - minimo;  
+    var numerosPosibles = maximo - minimo;
     var random = Math.random() * (numerosPosibles + 1);
     random = Math.floor(random);  // necesito un numero entero
     return minimo + random;
 }
- 
-const poolPalabras = ['PALABRAS', 'PARA', 'PRUEBA', 'JUEGO', 'COLGADO', 'PARA', 'ELEGIR','USUARIO', 'PENDIENTE', 'VERSION', 'FINAL']; 
+const poolPalabras = ['PALABRAS', 'PARA', 'PRUEBA', 'JUEGO', 'COLGADO', 'PARA', 'ELEGIR', 'USUARIO', 'PENDIENTE', 'VERSION', 'FINAL'];
 // total de palabras aún no definido
 
 //INICIO INTERACCION
@@ -35,23 +31,22 @@ saludaUsuario();
 alert('Estas son las bases de un juego de palabras que programaré');
 
 let eligePersonaje = parseInt(prompt('Habrá personajes para elegir, elige entre personajes 1, 2 o 3'));
-
-switch (eligePersonaje){
+switch (eligePersonaje) {
     case 1:
         alert(`Excelente, elegiste el personaje 1: ${Object.values(perso1)}`);
-    break;    
+        break;
     case 2:
         alert(`Excelente, elegiste el personaje 2: ${Object.values(perso2)}`);
-    break; 
+        break;
     case 3:
         alert(`Excelente, elegiste el personaje 3: ${Object.values(perso3)}`);
-    break; 
+        break;
     default:
         alert('no elegiste una opción válida');
 
 }
 alert('A modo de demostración generaré un número al azar entre 0-10 , después lo usaremos para elegir tu palabra desde un pool');
-let numeroAleatorio = indexRandom(0,10);
+let numeroAleatorio = indexRandom(0, 10);
 alert(`El número aleatorio generado para tí es ${numeroAleatorio}`);
 
 let palabra = poolPalabras[numeroAleatorio];
@@ -64,31 +59,30 @@ let letraIngresada = prompt('Ahora ingresa una letra y te contaré cuántas vece
 let letraMayus = letraIngresada.toUpperCase();
 let ubicaciones = [];
 
-if (isNaN(letraMayus)){  //si no es un número, busca en cada index y agrega su posición a otro array 
+if (isNaN(letraMayus)) {  //si no es un número, busca en cada index y agrega su posición a otro array 
     let indice = 0;
-    while (indice < letras.length +1) {
-        if (letras[indice] == letraMayus){
+    while (indice < letras.length + 1) {
+        if (letras[indice] == letraMayus) {
             ubicaciones.push(indice);
             indice++;
         }
-      
         else {
             indice++;
         }
     }
     let numeroDeVeces = ubicaciones.length;  // ahora sabremos cuántas veces está la letra y luego en qué posiciones.
 
-    let posicionesParaUsuario = ubicaciones.map(function(suma){return suma +1}); //+1 para que usuario vea posiciones que conoce
-    
+    let posicionesParaUsuario = ubicaciones.map(function (suma) { return suma + 1 }); 
+    //+1 a cada elemento para evitar el cero, el usuario vea posiciones que conoce
 
-        if (numeroDeVeces == 0){
-           alert(`la letra ${letraMayus} no se encuentra en tu palabra`); 
-        } else{
-            
-    alert(`la letra ${letraMayus} se encuentra ${numeroDeVeces} vez(ces) y en posición(es) ${posicionesParaUsuario} de izquierda a derecha`);   
-        }
-}    
-else { 
-            alert('el valor que ingresaste es un número, no una letra');
+    if (numeroDeVeces == 0) {
+        alert(`la letra ${letraMayus} no se encuentra en tu palabra`);
+    } else {
+
+        alert(`la letra ${letraMayus} se encuentra ${numeroDeVeces} vez(ces) y en posición(es) ${posicionesParaUsuario} de izquierda a derecha`);
+    }
 }
-alert ('Gracias por tu visita, hasta la próxima');
+else {
+    alert('el valor que ingresaste es un número, no una letra');
+}
+alert('Gracias por tu visita, hasta la próxima');
