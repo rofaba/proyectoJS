e35bdbec66844e1719216ae8508685d37bd37660
rofaba <rodrigofaure@gmail.com>
2022-03-17 17:09:54 -0300

    Entrega segundo desafío complementario

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// //Simulador interactivo JS: Juego de "El Colgado".
+//Simulador interactivo JS: Juego de "El Colgado".
 
-//CLASE PERSONAJE creación de personajes para Proyecto Final.
+// DESAFIO COMPLEMENTARIO 2 INCLUYENDO OBJETOS FUNCIONES ARRAYS Y METODOS
+
+//creación de personajes .
 
 class Personaje {
     constructor(nombre, edad, ocupacion) {
@@ -9,39 +11,34 @@ class Personaje {
         this.ocupacion = ocupacion; 
     }
 }
-const perso1 = new Personaje ('Homero', '39 años', 'Técnico Nuclear');
-const perso2 = new Personaje ('Bart', '10 años', 'Estudiante');
-const perso3 = new Personaje ('Lisa', '8 años', 'Estudiante');
+const perso1 = new Personaje ('Carlos', ' 20 años ', ' Jugador Empedernido');
+const perso2 = new Personaje ('Roberto', ' 15 años ', ' Estudiante');
+const perso3 = new Personaje ('Claudia ', ' 29 años ', ' Profesora');
 
 const poolPersonajes = [perso1, perso2, perso3];
 
-// DESAFIO COMPLEMENTARIO 2 INCLUYENDO OBJETOS FUNCIONES ARRAYS Y METODOS
-
-function indexRandom(minimo, maximo) {   //los argumentos se establerán entre 0 y el número de palabras a incluir 
-    var numerosPosibles = maximo + minimo;  
+function saludaUsuario() {
+    alert(`Hola Pablo, hoy tengo algo nuevo que mostrarte`)
+}
+function indexRandom(minimo, maximo) {   //los argumentos se establerán entre 0 y el número total de palabras 
+    var numerosPosibles = maximo - minimo;  
     var random = Math.random() * (numerosPosibles + 1);
     random = Math.floor(random);  // necesito un numero entero
     return minimo + random;
 }
-    
-//esta funcion compara cada una de las letras de la palabra con la que debe buscar
-function comparaLetras(letra){
-    return letra == letraMayus;
-}
-
-const poolPalabras = ['PALABRAS', 'PRUEBA', 'JUEGO', 'COLGADO', 'PARA', 'ELEGIR','USUARIO', 'PENDIENTE', 'VERSION', 'FINAL']; 
+ 
+const poolPalabras = ['PALABRAS', 'PARA', 'PRUEBA', 'JUEGO', 'COLGADO', 'PARA', 'ELEGIR','USUARIO', 'PENDIENTE', 'VERSION', 'FINAL']; 
 // total de palabras aún no definido
 
-const coincidencias = [];
 //INICIO INTERACCION
+saludaUsuario();
+alert('Estas son las bases de un juego de palabras que programaré');
 
-alert('Hola. Hoy te mostraré un juego de palabras que estoy empezando a programar');
-
-let eligePersonaje = parseInt(prompt('Tengo un juego de palabras. Elige entre personajes 1, 2 o 3'));
+let eligePersonaje = parseInt(prompt('Habrá personajes para elegir, elige entre personajes 1, 2 o 3'));
 
 switch (eligePersonaje){
     case 1:
-        alert(`Excelente, elegiste el personaje 1: ${Object.values(perso2)}`);
+        alert(`Excelente, elegiste el personaje 1: ${Object.values(perso1)}`);
     break;    
     case 2:
         alert(`Excelente, elegiste el personaje 2: ${Object.values(perso2)}`);
@@ -53,23 +50,23 @@ switch (eligePersonaje){
         alert('no elegiste una opción válida');
 
 }
-alert('A modo de demostración generaré un número al azar entre 0-10 , tengo solo 10 palabras por ahora');
+alert('A modo de demostración generaré un número al azar entre 0-10 , después lo usaremos para elegir tu palabra desde un pool');
 let numeroAleatorio = indexRandom(0,10);
-alert(`El número aleatorio generado es ${numeroAleatorio}`);
+alert(`El número aleatorio generado para tí es ${numeroAleatorio}`);
 
 let palabra = poolPalabras[numeroAleatorio];
-alert(`tu palabra aleatoria es ${palabra}`);
+alert(`Con ese número tu palabra aleatoria corresponde a ${palabra}`);
 
-//transformamos el string en un array (spread operator)
+//transformamos el string en un array donde podremos buscar (spread operator)
 const letras = [...palabra];
 
-let letraIngresada = prompt('Ahora ingresa una letra y te contaré en qué posición está ubicada dentro de tu palabra');
+let letraIngresada = prompt('Ahora ingresa una letra y te contaré cuántas veces y en qué posición está ubicada dentro de tu palabra');
 let letraMayus = letraIngresada.toUpperCase();
 let ubicaciones = [];
 
-if (isNaN(letraMayus)){
+if (isNaN(letraMayus)){  //si no es un número, busca en cada index y agrega su posición a otro array 
     let indice = 0;
-    while (indice < letras.length) {
+    while (indice < letras.length +1) {
         if (letras[indice] == letraMayus){
             ubicaciones.push(indice);
             indice++;
@@ -79,10 +76,19 @@ if (isNaN(letraMayus)){
             indice++;
         }
     }
-    let numeroDeVeces = ubicaciones.length;
-    alert(`la letra ${letraMayus} se encuentra ${numeroDeVeces} veces y en posición ${ubicaciones}`);   
+    let numeroDeVeces = ubicaciones.length;  // ahora sabremos cuántas veces está la letra y luego en qué posiciones.
+
+    let posicionesParaUsuario = ubicaciones.map(function(suma){return suma +1}); //+1 para que usuario vea posiciones que conoce
+    
+
+        if (numeroDeVeces == 0){
+           alert(`la letra ${letraMayus} no se encuentra en tu palabra`); 
+        } else{
+            
+    alert(`la letra ${letraMayus} se encuentra ${numeroDeVeces} vez(ces) y en posición(es) ${posicionesParaUsuario} de izquierda a derecha`);   
+        }
 }    
 else { 
             alert('el valor que ingresaste es un número, no una letra');
 }
-             // avisamos que el valor ingresado es un numero   
+alert ('Gracias por tu visita, hasta la próxima');
