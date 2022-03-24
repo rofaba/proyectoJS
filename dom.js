f0289004a9d282885a29e1e91bd647529a0b715c
rofaba <rodrigofaure@gmail.com>
2022-03-24 16:13:42 -0300

    estilos

--- dom.js
+++ dom.js
@@ -5,22 +5,22 @@
 
 const tarjeta = document.getElementById("probando");
 
-function resaltar(){
+function resaltar() {
     tarjeta.style.backgroundColor = 'yellow';
 
-    };
+};
 
-function normalizar(){
+function normalizar() {
     tarjeta.style.backgroundColor = 'rgb(160, 138, 138)';
 }
 
 let fotoSelect = document.getElementById("seleccion");
-let nombreNuevo =document.getElementById('nombreSeleccionado');
-let nuevoTexto =document.getElementById('textoSeleccionado'); 
+let nombreNuevo = document.getElementById('nombreSeleccionado');
+let nuevoTexto = document.getElementById('textoSeleccionado');
 
-function seleccionarPersonaje(){
-    fotoSelect.src="imagenes/personajeBart.jpg";
-    nombreNuevo.innerHTML ='Bart Simpson';
+function seleccionarPersonaje() {
+    fotoSelect.src = "imagenes/personajeBart.jpg";
+    nombreNuevo.innerHTML = 'Bart Simpson';
     nuevoTexto.innerHTML = 'Seleccionaste Bart Para Jugar';
 
 }
