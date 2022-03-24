52f8dae1c69cfab6fd14e215bfeca1a154d639ef
rofaba <rodrigofaure@gmail.com>
2022-03-24 16:01:14 -0300

    desafio manipulando DOM

---
+++ dom.js
@@ -0,0 +1,35 @@
+//DESAFIO COMPLEMENTARIO MODIFICANDO EL DOM
+//simulación proceso de selección de personaje en el juego
+
+// alert("jsfunciona");
+
+const tarjeta = document.getElementById("probando");
+
+function resaltar(){
+    tarjeta.style.backgroundColor = 'yellow';
+
+    };
+
+function normalizar(){
+    tarjeta.style.backgroundColor = 'rgb(160, 138, 138)';
+}
+
+let fotoSelect = document.getElementById("seleccion");
+let nombreNuevo =document.getElementById('nombreSeleccionado');
+let nuevoTexto =document.getElementById('textoSeleccionado'); 
+
+function seleccionarPersonaje(){
+    fotoSelect.src="imagenes/personajeBart.jpg";
+    nombreNuevo.innerHTML ='Bart Simpson';
+    nuevoTexto.innerHTML = 'Seleccionaste Bart Para Jugar';
+
+}
+
+tarjeta.addEventListener("mouseover", resaltar);
+
+tarjeta.addEventListener("mouseleave", normalizar);
+
+tarjeta.addEventListener("click", seleccionarPersonaje);
+
+
+
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //Simulador interactivo JS: Juego de "El Colgado".
-// PRIMERA ENTREGA FINAL
 
 //creación de personajes
 class Personaje {
@@ -14,103 +13,107 @@ const perso2 = new Personaje('Bart ', ' 10 años ', ' Niño Incomprendido');
 const perso3 = new Personaje('Lisa ', ' 8 años ', ' Niña Genio');
 const poolPersonajes = [perso1, perso2, perso3];
 
-function saludaUsuario() {
-    alert('Hola Bienvenido al Juego del Ahorcado')
-}
-function indexRandom(minimo, maximo) {   //número al azar para extraer palabra desde array 
-    var numerosPosibles = maximo - minimo;
-    var random = Math.random() * (numerosPosibles + 1);
-    random = Math.floor(random);  // transformación a numero entero
-    return minimo + random;
-}
-const poolPalabras = ['PALABRAS', 'PARA', 'PRUEBA', 'JUEGO', 'COLGADO', 'PARA', 'ELEGIR', 'USUARIO', 'PENDIENTE', 'VERSION', 'FINAL'];
-
-saludaUsuario();
-alert('Lo primero es que elijas tu personaje, tengo 3 disponibles');
-
-let eligePersonaje = parseInt(prompt('Por favor, indìcame cuál prefieres: 1, 2 ó 3'));
-switch (eligePersonaje) {
-    case 1:
-        alert(`Muy bien, elegiste a: ${Object.values(perso1)}`);
-        break;
-    case 2:
-        alert(`Excelente, elegiste a: ${Object.values(perso2)}`);
-        break;
-    case 3:
-        alert(`Bien hecho, elegiste a: ${Object.values(perso3)}`);
-        break;
-    default:
-        alert('No elegiste personaje, te asignaré uno de todos modos.');
-
-}
-let numeroAleatorio = indexRandom(0, 10);
-let palabra = poolPalabras[numeroAleatorio];
-
-//transformamos el string en un array
-const letras = [...palabra];
-
-// creamos un array similar que contiene "__" en similar numero para llenar luego
-
-const llenandoPalabra = [];
-for (let i = 0; i < letras.length; i++) {
-    llenandoPalabra.push(' __ ');
-}
-console.log(letras);
-
-alert(`Ya tengo tu palabra secreta ${llenandoPalabra}, veamos si la puedes adivinar`);
-let palabraCompleta = llenandoPalabra.indexOf(' __ ');
-let vidas = 7;
-
-while (vidas != 0) {
-
-    letraIngresada = prompt('Vamos, ingresa una letra');
-    let letraMayus = letraIngresada.toUpperCase();
-    let contadorAcierto = [];
-
-    if (isNaN(letraMayus)) {  //si no es un número, busca en cada index y agrega su posición a otro array 
-
-        let indice = 0;
-        while (indice < letras.length + 1) {
-            if (letras[indice] == letraMayus) {
-                contadorAcierto.push(indice);
-                llenandoPalabra[indice] = letraMayus;
-                indice++;
-
-            }
-            else {
-                indice++;
-
-            }
-        }
-        if (contadorAcierto.length == 0) {
-            vidas--
-            alert(`la letra ${letraMayus} no se encuentra en tu palabra`);
-            if (vidas == 0) {
-                alert(`Se te acabaron las vidas, lo siento, tu palabra era ${palabra}`)
-            }
-            else {
-                alert(`Te quedan ${vidas} intentos más`)
-            }
-        } else {
-            alert(`Tu palabra: ${llenandoPalabra}`);
-        }
-
-        if ((llenandoPalabra.indexOf(' __ ') != -1) && (vidas != 0)) {
-
-        } else {
-            if (vidas != 0) {
-                alert(`GANASTE, tu palabra ${palabra} está completa!!!`)
-                indice = 50; // termino de ciclo alertas/prompt
-                vidas = 0 // termino de ciclo alertas/prompt
-            }
-
-        }
+    function saludaUsuario() {
+        alert('Hola Bienvenido al Juego del Ahorcado')
     }
-
-    else {
-        alert('el valor que ingresaste es un número, no una letra');
+    function indexRandom(minimo, maximo) {   //número al azar para extraer palabra desde array 
+        var numerosPosibles = maximo - minimo;
+        var random = Math.random() * (numerosPosibles + 1);
+        random = Math.floor(random);  // transformación a numero entero
+        return minimo + random;
     }
+    const poolPalabras = ['PALABRAS', 'PARA', 'PRUEBA', 'JUEGO', 'COLGADO', 'PARA', 'ELEGIR', 'USUARIO', 'PENDIENTE', 'VERSION', 'FINAL'];
+
+    saludaUsuario();
+    alert('Lo primero es que elijas tu personaje, tengo 3 disponibles');
+
+    let eligePersonaje = parseInt(prompt('Por favor, indìcame cuál prefieres: 1, 2 ó 3'));
+    switch (eligePersonaje) {
+        case 1:
+            alert(`Muy bien, elegiste a: ${Object.values(perso1)}`);
+            break;
+        case 2:
+            alert(`Excelente, elegiste a: ${Object.values(perso2)}`);
+            break;
+        case 3:
+            alert(`Bien hecho, elegiste a: ${Object.values(perso3)}`);
+            break;
+        default:
+            alert('No elegiste personaje, te asignaré uno de todos modos.');
+
+    }
+    let numeroAleatorio = indexRandom(0, 10);
+    let palabra = poolPalabras[numeroAleatorio];
+
+    //transformamos el string en un array
+    const letras = [...palabra];
+
+    // creamos un array similar que contiene "__" en similar numero para llenar luego
+
+    const llenandoPalabra = [];
+    for (let i = 0; i < letras.length; i++) {
+        llenandoPalabra.push(' __ ');
+    }
+    console.log(letras);
+
+
+
+    alert(`Ya tengo tu palabra secreta ${llenandoPalabra}, veamos si la puedes adivinar`);
     let palabraCompleta = llenandoPalabra.indexOf(' __ ');
-}
+    let vidas = 7;
 
-alert('Gracias por jugar, hasta la próxima');
+    while (vidas != 0) {
+
+        letraIngresada = prompt('Vamos, ingresa una letra');
+        let letraMayus = letraIngresada.toUpperCase();
+        let contadorAcierto = [];
+
+        if (isNaN(letraMayus)) {  //si no es un número, busca en cada index y agrega su posición a otro array 
+
+            let indice = 0;
+            while (indice < letras.length + 1) {
+                if (letras[indice] == letraMayus) {
+                    contadorAcierto.push(indice); //guarda cada index de las coincidencias
+                    llenandoPalabra[indice] = letraMayus; //acá reemplaza la letra
+                    indice++;
+
+                }
+                else {
+                    indice++;
+
+                }
+            }
+            if (contadorAcierto.length == 0) {  // si aciertos son cero la letra no está en palabra
+                vidas--
+                alert(`la letra ${letraMayus} no se encuentra en tu palabra`);
+                if (vidas == 0) { //revisa si quedan vidas para jugar
+                    alert(`Se te acabaron las vidas, lo siento, tu palabra era ${palabra}`)
+                }
+                else {
+                    alert(`Te quedan ${vidas} intentos más`)
+                }
+            } else {
+                alert(`Tu palabra: ${llenandoPalabra}`);
+            }
+
+            if ((llenandoPalabra.indexOf(' __ ') != -1) && (vidas != 0)) {
+
+            } else {
+                if (vidas != 0) {
+                    alert(`GANASTE, tu palabra ${palabra} está completa!!!`)
+                    indice = 50; // termino de ciclo alertas/prompt
+                    vidas = 0 // termino de ciclo alertas/prompt
+                }
+
+            }
+        }
+
+        else {
+            alert('el valor que ingresaste es un número, no una letra');
+        }
+        let palabraCompleta = llenandoPalabra.indexOf(' __ ');
+    }
+
+    alert('Gracias por jugar, hasta la próxima');
+
+    
