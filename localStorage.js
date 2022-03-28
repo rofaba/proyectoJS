367ac4035f1794bda5e1f3e59c11a05f64c41856
rofaba <rodrigofaure@gmail.com>
2022-03-28 15:49:49 -0300

    interacciones con DOM

--- localStorage.js
+++ localStorage.js
@@ -1,16 +1,16 @@
 //DESAFIO LOCAL STORAGE
 //guarda número de intentos que quedan
-function guardarVidas (vidas, valor) {
-    localStorage.setItem( vidas, valor);
-    }
-    
-    //lee el número de intentos que quedan
-    function leerVidas (vidas) {
-    let vidasGuardadas = localStorage.getItem (vidas);
+function guardarVidas(vidas, valor) {
+    localStorage.setItem(vidas, valor);
+}
+
+//lee el número de intentos que quedan
+function leerVidas(vidas) {
+    let vidasGuardadas = localStorage.getItem(vidas);
     return parseInt(vidasGuardadas)
-    }
-    
-    //limpia el LocalStorage para un nuevo juego
-    function limpiarStorage(){
+}
+
+//limpia el LocalStorage para un nuevo juego
+function limpiarStorage() {
     localStorage.clear();
-    }
+}
--- script.js
+++ script.js
@@ -1,111 +1,65 @@
 
-/* Se tiene poolPalabras como Array base del juego, con la función indexRandom obtenemos un número entero al azar y con el mismo se selecciona una de las palabras del array base, con esa palabra se juega */
+/* Cree [poolPalabras] como Array base del juego, con la función (indexRandom) obtengo un número entero al azar y con ese número selecciono la palabras para jugar desde el array base */
 
 const poolPalabras = ['palabras', 'para', 'prueba', 'juego', 'colgado'];
 
-function indexRandom(minimo, maximo) {
+function indexRandom(minimo, maximo) {    //fx reutilizable
     var numerosPosibles = maximo - minimo;
     var random = Math.random() * (numerosPosibles + 1);
-    random = Math.floor(random);  // transformación a numero entero
+    random = Math.floor(random);         // transformación a nº entero
     return minimo + random;
 }
 
 let numeroAleatorio = indexRandom(0, 5);
 let palabraSeleccionada = poolPalabras[numeroAleatorio];
-const letrasArray = [...palabraSeleccionada]; //transformamos el string en un array
+const letrasArray = [...palabraSeleccionada]; //transform string en array
 
-/* creamos un array con igual número de letras que la palabra seleccionada, lleno de guiones para completar. Lo mostramos al usuario en H2 palabraAdivina */
+/* creo un array de guiones similar a la palabra para ser completado, se muestra al usuario en <H2> palabraAdivina */
 
-let completandoPalabra = []; 
-       for (let i = 0; i < letrasArray.length; i++) {
-        completandoPalabra.push(' _ ');
-    }
+let completandoPalabra = [];
+for (let i = 0; i < letrasArray.length; i++) {
+    completandoPalabra.push(' _ ');
+}
 
-let palabraGuiones = document.getElementById('palabraAdivina');
-palabraGuiones.innerHTML = completandoPalabra;  
+document.getElementById('palabraAdivina').innerHTML = completandoPalabra.join('');  //sacamos las comas
 
-//XXX
+//me muestro la palabra secreta para las pruebas del código
 let probando = document.getElementById('probando');
 probando.innerHTML = letrasArray;
-//XXX
-
-//Variables globales
-let vidasPendientes;
-let contadorAciertos = 0;
 
 //EMPEZANDO JUEGO
 const jugar = document.getElementById('botonInicio');
-jugar.addEventListener('click', jugando);
+jugar.addEventListener('click', jugando); //lanza la función
 let vidasRestantes = 7;
 
-function jugando(){
-let letraIngresada = document.getElementById('letraUsuario');
-let letra = letraIngresada.value;
-let i=0;
-let laLetraNoEsta = true;
+function jugando() {
+    let letraIngresada = document.getElementById('letraUsuario');
+    let letra = letraIngresada.value;
+    let i = 0;
+    let laLetraNoEsta = true; //para ciclo cuenta de vidas
 
-//verifica si la letra existe en palabra
-       
-for (let i = 0; i < letrasArray.length; i++) {
-    if (letrasArray[i] == letraIngresada.value) {
-        completandoPalabra[i] = letraIngresada.value;
-        laLetraNoEsta = false;
-        document.getElementById('palabraAdivina').innerHTML = completandoPalabra;
-        if((completandoPalabra.includes(' _ ')) != true){
-            alert('GANASTE, tu palabra está completa')
+    //verifica si la letra existe en palabra
+
+    for (let i = 0; i < letrasArray.length; i++) {
+        if (letrasArray[i] == letraIngresada.value) {
+            completandoPalabra[i] = letraIngresada.value;
+            laLetraNoEsta = false;
+            document.getElementById('palabraAdivina').innerHTML = completandoPalabra.join('');
+            document.getElementById('mensajeUsuario').innerHTML = "Muy bien, tenemos coincidencias"
+            document.getElementById('segundoMensaje').innerHTML = "Ingresa la siguiente letra"
+
+            if ((completandoPalabra.includes(' _ ')) != true) {
+                alert('GANASTE, tu palabra está completa')
+            }
         }
-    } 
-}
-if (laLetraNoEsta) {
-    vidasRestantes--;
-    document.getElementById('numeroVidas').innerHTML = vidasRestantes;
+    }
+    if (laLetraNoEsta) {
+        vidasRestantes--;
+        document.getElementById('numeroVidas').innerHTML = vidasRestantes;
+        document.getElementById('mensajeUsuario').innerHTML = "Esa letra no está en tu palabra"
+        document.getElementById('segundoMensaje').innerHTML = "En la próxima tendrás mejor suerte, vamos!"
     }
     if (vidasRestantes == 0) {
-       alert('tus intentos se acabaron, PERDISTE'); 
+        alert('tus intentos se acabaron, PERDISTE');
     }
 }
-
-
-
-
-// if (contadorAciertos == 0){
-//     let mensaje = document.getElementById('mensajeUsuario')
-//     let mensajeDos = document.getElementById('segundoMensaje');
-            
-//     mensaje.innerHTML = `la letra no se encuentra en tu palabra`;
-//     mensajeDos = innerHTML = 'Ingresa otra letra';        
-//     intentosPendientes--;
-//     
-// }
-      
-// let palabraSecreta = document.getElementById('palabraAdivina');
-// palabraSecreta.innerHTML = llenandoPalabra;  
-
-
-// //revisa aciertos vs vidas
-//     if (contadorAciertos == 0) {  // la letra no está en la palabra
-//             vidas--
-//             let mensaje = document.getElementById('mensajeUsuario')
-//             let mensajeDos = document.getElementById('segundoMensaje');
-//             let loose = document.getElementById('numeroVidas');
-//             mensaje.innerHTML = `la letra ${letraIngresada} no se encuentra en tu palabra`;
-//             mensajeDos = innerHTML = 'Ingresa otra letra';
-//             loose.innerHTML = vidas - 1;
-//         }
-//         else {
-//             palabraSecreta.innerHTML = llenandoPalabra;
-//         }
-
-
-// //Informa avance del juego 
-
-//         if (vidas == 0) { //el juego terminò por vidas en cero
-//             mensaje.innerHTML = 'No te quedan más vidas';
-//             mensajeDos.innerHTML = `Fin del juego, tú palabra era ${palabra}`
-//         }
-//         else { //el juego terminó por palabra completa
-
-//             let cierre = document.getElementById('segundoMensaje');
-//             cierre.innerHTML = `GANASTE, tu palabra ${palabra} está completa!!!`;
-//             indice = 50; // termino de ciclo 
-//             vidas = 0; // termino de ciclo
